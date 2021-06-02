c07766e89be4482791e3b8034759691714be6ae9
Kelton <kelton@orth.org>
2021-06-01 22:13:42 -0700

    Added tests

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+"""
+Tests for calc app
+"""
+
+import pytest
+from calculator import *
+
+class TestCalcApp:
+
+    def test_add(self):
+        assert add(5, 5) == 10
+
+
+    def test_sub(self):
+        assert sub(10, 5) == 5
+
+
+    def test_mul(self):
+        assert mul(5, 5) == 25
